48c0823c08149167268e7e6e310b34833b87243b
daisy-mwale <mwaledaisy46@gmail.com>
2026-05-29 01:12:07 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Cloud GitOps pipeline is working",
+    version: "v1"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+    
